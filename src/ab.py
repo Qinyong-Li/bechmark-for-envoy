42f1afdb4046c7cfc3f9752ca3af4b57980cd01c
Qinyong-Li <18398079772@163.com>
2021-04-01 09:55:38 +0800

    add muti-url ab

--- src/ab.py
+++ src/ab.py
@@ -1,49 +1,56 @@
 import subprocess
 import sys
 import socket
+import os
+
 
 def help():
     print("pls input as fallow")
     print("python3 ab.py [$IP] [$PORT] [$PATH]")
     print("PATH: the path of url.txt")
 
-def sendRequests(ip_port,urls_path):
-    requests=[1,10,100,1000]
-    concurrency=[1,10,100,1000]
-    start_flag="001"
-    finished_flag="010"
-    close_flag="011"
-    soc=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+def sendRequests(ip_port, urls_path):
+    requests = [1, 10, 100, 1000]
+    concurrency = [1, 10, 100, 1000]
+    start_flag = "001"
+    finished_flag = "010"
+    close_flag = "011"
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     soc.connect(ip_port)
-    
+
     for c in concurrency:
         for r in requests:
-            filename=str(c)+"-"+str(r*c)+".data"
+            filename = str(c)+"-"+str(r*c)+".data"
             soc.send(filename.encode("utf-8"))
-            msg_rev=soc.recv(1024).decode("utf-8")
-            if msg_rev !=start_flag:
+            msg_rev = soc.recv(1024).decode("utf-8")
+            if msg_rev != start_flag:
                 return
-            ab_filename=str(c)+"-"+str(c*r)+".txt"
-            command="ab -c "+str(c)+" -n "+str(r*c)+" -L "+urls_path+">"+ab_filename
-            prc=subprocess.Popen(command,shell=True)
+            ab_filename = str(c)+"-"+str(c*r)+".txt"
+            path = os.path.dirname(__file__)+"/../ "
+            command = path+"ab -c " + \
+                str(c)+" -n "+str(r*c)+" -L "+urls_path+">"+ab_filename
+            prc = subprocess.Popen(command, shell=True)
             prc.wait()
             soc.send(finished_flag.encode("utf-8"))
     soc.send(close_flag.encode("utf-8"))
 
+
 def main():
-    argv=sys.argv
-    length=len(argv)
-    if length==2 and argv[1]=="--help":
+    argv = sys.argv
+    length = len(argv)
+    if length == 2 and argv[1] == "--help":
         help()
-    elif length==4:
-        ip=argv[1]
-        port=argv[2]
-        urls_path=argv[3]
-        ip_port=(ip,port)
-        sendRequests(ip_port,urls_path)
+    elif length == 4:
+        ip = argv[1]
+        port = argv[2]
+        urls_path = argv[3]
+        ip_port = (ip, port)
+        sendRequests(ip_port, urls_path)
     else:
         print("ab.py: wrong number of arguments")
         print()
         help()
 
-main()
+
+print(os.path.dirname(__file__))
--- src/perf.py
+++ src/perf.py
@@ -2,30 +2,31 @@ import sys
 import subprocess
 import socket
 
+
 def help():
     print("python3 perf_record.py [$IP] [$PORT] [$PID] [$PADDWORD]")
     print("PID: pid of envoy")
 
-def record_data(ip_port,port,password):
-    soc=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+def record_data(ip_port, port, password):
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     soc.bind(ip_port)
     soc.listen(1)
-    con,address=soc.accept()
-    start_flag="001"
-    finished_flag="010"
-    close_flag="011"
+    con, address = soc.accept()
+    start_flag = "001"
+    finished_flag = "010"
+    close_flag = "011"
     while True:
-        filename=con.recv().decode("utf-8")
+        filename = con.recv().decode("utf-8")
         con.send(start_flag.encode("utf-8"))
-        command="sudo perf record -e cycles -p "+port
-        proc=subprocess.Popen("echo %s|sudo -S %s"(password,command))
-        msg_rev=con.recv().decode("utf=8")
-        if msg_rev!=finished_flag:
+        command = "sudo perf record -e cycles -p "+port
+        proc = subprocess.Popen("echo %s|sudo -S %s"(password, command))
+        msg_rev = con.recv().decode("utf=8")
+        if msg_rev != finished_flag:
             exit()
         proc.terminate()
-        if msg_rev==close_flag:
+        if msg_rev == close_flag:
             break
-
 
 
 main()
